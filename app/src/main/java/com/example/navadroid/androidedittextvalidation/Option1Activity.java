48f0a9251c3e1eeebda5ad1fb44f1411cfd2a86e
Mindmie <dreamiing.allday@gmail.com>
2017-11-29 01:13:26 +0700

    password validation

--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option1Activity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Option1Activity extends AppCompatActivity {
 
     private EditText etName;
@@ -34,12 +37,16 @@ public class Option1Activity extends AppCompatActivity {
         findViewById(R.id.btn_validate1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validateEditText()) {
+                if(validateEditText()&& validatePassword()) {
                     Toast.makeText(Option1Activity.this, "Okay. You are good to go.", Toast.LENGTH_SHORT).show();
                     // SnackBar?
                 }
+                else{
+                    Toast.makeText(Option1Activity.this, "Please try again.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
 
         etName.addTextChangedListener(new TextWatcher() {
             @Override
@@ -71,12 +78,44 @@ public class Option1Activity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                validatePassword();
+
                 // TODO: add your Password validation here
             }
         });
 
-//        etPhone.addTextChangedListener(...);
-//        etEmail.addTextChangedListener(...);
+        etPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     // To validate all EditTexts
@@ -86,8 +125,27 @@ public class Option1Activity extends AppCompatActivity {
             etName.setError("Required");
             isValidated = false;
         }
+
         // TODO: add your EditText validation here
 
         return isValidated;
+    }
+
+    // to validate password
+    private boolean validatePassword(){
+
+        boolean isValidated = true;
+        if (etPwd.getText().toString().length() < 6 || etPwd.getText().toString().length() > 10 ){
+            etPwd.setError("Character should be 6 - 10");
+            isValidated = false;
+        }
+        else if (etPwd.getText().toString().matches("[!@#$%^&*?]+")){
+            etPwd.setError("Character should be a-z ,A-Z , 0-9");
+            isValidated = false;
+        }
+
+        return isValidated;
+
+
     }
 }
